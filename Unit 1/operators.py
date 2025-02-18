82ac77c465e4edd9f9e61cc046065a1934eecffb
Kaisamp55 <ksample-walker418@boyslatin.net>
2025-02-18 19:24:54 +0000

    Complete notes on operators

---
+++ Unit 1/operators.py
@@ -0,0 +1,60 @@
+# Data manipulation  - Operators
+
+# In python we have operators, that
+# allow us to change data.
+
+# Assignment operators
+# assignment operators use some combination of the equal
+# Comparison operators
+
+name = 'Kailin' # the equal sign is the opeator
+willItSnow = True
+numberOfSandwiches = 10
+bankaccount = 100000
+
+# Comparison operators
+# comparisons operators allow us to evaluate data
+#based on several facotors
+
+10 < 20 # is 10 less than 20
+
+20.34 >= 0 # is 20.34 greater than or equal to 0
+
+100 == 100 # is 100 the SAME AS 100
+
+100 != 10 # is 100 not the SAME AS 10
+
+# Arithmatic operators
+# the arithmatic operators allow us to perform
+# math with- specifically with the intereger and float
+# data types
+
+10 + 10 # addition
+12 - 2 # subtraction
+
+# Logical operators
+# we use logical operators to evalute if data is true
+# or false
+
+password = '123'
+securityQuestion = 'Sample'
+
+mathAnswer = 4
+# and - used to compare to conditions that are both true
+
+# if both conditions below are true, we will have the value
+# of true
+
+password == '123' and 4 == mathAnswer
+
+# or - used to copare conditions, But only one needs to
+# to be true in order for the output to be true
+
+password = '123' or 2+3 == mathAnswer
+
+# not - used to compare conditions. BUT will
+# output the opposite result- meaning if two things are true
+# we are going to get false
+
+# whatever the real result is, will become the opposite
+not(password == '123' and 2+2 ==mathAnswer)
